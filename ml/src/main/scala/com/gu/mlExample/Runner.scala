8d8f9e6d7d7878918e677e042560558769150b1d
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-11-14 18:22:33 +0000

    Basic pipeline example is working.

--- ml/src/main/scala/com/gu/mlExample/Runner.scala
+++ ml/src/main/scala/com/gu/mlExample/Runner.scala
@@ -1,9 +1,10 @@
 package com.gu.mlExample
 
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.feature.Tokenizer
-import org.apache.spark.sql.{SparkSession}
-import org.apache.spark.{SparkConf}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.SparkConf
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.linalg.{Vector, Vectors}
+import org.apache.spark.ml.param.ParamMap
 
 
 object Runner {
@@ -14,31 +15,63 @@ object Runner {
 
   val sparkSession = SparkSession.builder().config(conf).getOrCreate()
 
-  val tokenizer = new Tokenizer().setInputCol("text").setOutputCol("tokens")
 
-  val pipeline = new Pipeline().setStages(Array(tokenizer))
+  // Prepare training data from a list of (label, features) tuples.
+  val training = sparkSession.createDataFrame(Seq(
+    (1.0, Vectors.dense(0.0, 1.1, 0.1)),
+    (0.0, Vectors.dense(2.0, 1.0, -1.0)),
+    (0.0, Vectors.dense(2.0, 1.3, 1.0)),
+    (1.0, Vectors.dense(0.0, 1.2, -0.5))
+  )).toDF("label", "features")
 
-  val inputData = sparkSession.createDataFrame(Seq(
-    (0L, "a b c d e spark", 1.0),
-    (1L, "b d", 0.0),
-    (2L, "spark f g h", 1.0),
-    (3L, "hadoop mapreduce", 0.0),
-    (4L, "b spark who", 1.0),
-    (5L, "g d a y", 0.0),
-    (6L, "spark fly", 1.0),
-    (7L, "was mapreduce", 0.0),
-    (8L, "e spark program", 1.0),
-    (9L, "a e c l", 0.0),
-    (10L, "spark compile", 1.0),
-    (11L, "hadoop software", 0.0)
-  )).toDF("id", "text", "label")
+  // Create a LogisticRegression instance. This instance is an Estimator.
+  val lr = new LogisticRegression()
+  // Print out the parameters, documentation, and any default values.
+  println("LogisticRegression parameters:\n" + lr.explainParams() + "\n")
 
-  val model = pipeline.fit(inputData);
+  // We may set parameters using setter methods.
+  lr.setMaxIter(10)
+    .setRegParam(0.01)
 
-  val realData = sparkSession.createDataFrame(Seq((23, "example of hello world"))).toDF("id", "text")
+  // Learn a LogisticRegression model. This uses the parameters stored in lr.
+  val model1 = lr.fit(training)
+  // Since model1 is a Model (i.e., a Transformer produced by an Estimator),
+  // we can view the parameters it used during fit().
+  // This prints the parameter (name: value) pairs, where names are unique IDs for this
+  // LogisticRegression instance.
+  println("Model 1 was fit using parameters: " + model1.parent.extractParamMap)
 
-  val transformedData = model.transform(realData)
+  // We may alternatively specify parameters using a ParamMap,
+  // which supports several methods for specifying parameters.
+  val paramMap = ParamMap(lr.maxIter -> 20)
+    .put(lr.maxIter, 30)  // Specify 1 Param. This overwrites the original maxIter.
+    .put(lr.regParam -> 0.1, lr.threshold -> 0.55)  // Specify multiple Params.
 
-  transformedData.select("tokens").show()
+  // One can also combine ParamMaps.
+  val paramMap2 = ParamMap(lr.probabilityCol -> "myProbability")  // Change output column name.
+  val paramMapCombined = paramMap ++ paramMap2
+
+  // Now learn a new model using the paramMapCombined parameters.
+  // paramMapCombined overrides all parameters set earlier via lr.set* methods.
+  val model2 = lr.fit(training, paramMapCombined)
+  println("Model 2 was fit using parameters: " + model2.parent.extractParamMap)
+
+  // Prepare test data.
+  val test = sparkSession.createDataFrame(Seq(
+    (1.0, Vectors.dense(-1.0, 1.5, 1.3)),
+    (0.0, Vectors.dense(3.0, 2.0, -0.1)),
+    (1.0, Vectors.dense(0.0, 2.2, -1.5))
+  )).toDF("label", "features")
+
+  // Make predictions on test data using the Transformer.transform() method.
+  // LogisticRegression.transform will only use the 'features' column.
+  // Note that model2.transform() outputs a 'myProbability' column instead of the usual
+  // 'probability' column since we renamed the lr.probabilityCol parameter previously.
+  model2.transform(test)
+    .select("features", "label", "myProbability", "prediction")
+    .collect()
+    .foreach { case Row(features: Vector, label: Double, prob: Vector, prediction: Double) =>
+      println(s"($features, $label) -> prob=$prob, prediction=$prediction")
+    }
 
 }
